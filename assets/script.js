d6c235f229429cca7d1734fb23f7e2c1ebe5cb30
emilykidd3 <e.kidd61@yahoo.com>
2022-07-01 20:22:11 -0700

    added to divs and made dynamic script for past competitions

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,12 @@
 var addCompButton = document.querySelector('#add-comp-button')
+var trackerListDiv = document.querySelector("#tracker-list")
 
 addCompButton.addEventListener('click', function(){
      console.log("clicked!")
     })
+
+var addToList = function(){
+    trackerListDiv.textContent = "comp 1"
+}
+
+addToList();
