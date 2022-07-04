ae331760e92281bfc8d30a506cc5fb753d1a15a0
emilykidd3 <e.kidd61@yahoo.com>
2022-07-03 23:17:54 -0700

    fixed prevent default to not refresh, and updated list to be actual competitions not hard coded

--- assets/script.js
+++ assets/script.js
@@ -4,15 +4,17 @@ var trackerListDiv = document.querySelector("#tracker-list")
 var compArr = []
 
 addCompButton.addEventListener('click', function(e){
-    e.preventDefault
+    e.preventDefault()
      console.log("clicked!")
      var inputVal = document.querySelector('#add-comp-input').value
+     inputVal.value = ""
      compArr.push(inputVal)
      console.log(compArr)
+     addToList();
     })
 
 var addToList = function(){
-    trackerListDiv.textContent = "comp 1"
+    trackerListDiv.textContent = compArr
 }
 
 addToList();
